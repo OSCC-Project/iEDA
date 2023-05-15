ec38aea21fed228e08731a3cc7b9e6d57b7dd103
longshy <1253947628@qq.com>
2023-05-15 11:56:01 +0800

    fix getCapacitance

--- src/operation/iSTA/api/TimingIDBAdapter.cc
+++ src/operation/iSTA/api/TimingIDBAdapter.cc
@@ -1,16 +1,16 @@
 // ***************************************************************************************
 // Copyright (c) 2023-2025 Peng Cheng Laboratory
-// Copyright (c) 2023-2025 Institute of Computing Technology, Chinese Academy of Sciences
-// Copyright (c) 2023-2025 Beijing Institute of Open Source Chip
+// Copyright (c) 2023-2025 Institute of Computing Technology, Chinese Academy of
+// Sciences Copyright (c) 2023-2025 Beijing Institute of Open Source Chip
 //
 // iEDA is licensed under Mulan PSL v2.
-// You can use this software according to the terms and conditions of the Mulan PSL v2.
-// You may obtain a copy of Mulan PSL v2 at:
+// You can use this software according to the terms and conditions of the Mulan
+// PSL v2. You may obtain a copy of Mulan PSL v2 at:
 // http://license.coscl.org.cn/MulanPSL2
 //
-// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 //
 // See the Mulan PSL v2 for more details.
 // ***************************************************************************************
@@ -187,17 +187,8 @@ double TimingIDBAdapter::getCapacitance(int num_layer, double segment_length,
                     idb_layout->get_units()->get_micron_dbu();
   }
 
-  double lef_capacitance;
-  double lef_edge_capacitance;
-  if (idb_layout->get_units()->get_picofarads() == -1) {
-    lef_capacitance = routing_layer->get_capacitance();
-    lef_edge_capacitance = routing_layer->get_edge_capacitance();
-  } else {
-    lef_capacitance = routing_layer->get_capacitance() /
-                      idb_layout->get_units()->get_picofarads();
-    lef_edge_capacitance = routing_layer->get_edge_capacitance() /
-                           idb_layout->get_units()->get_picofarads();
-  }
+  double lef_capacitance = routing_layer->get_capacitance();
+  double lef_edge_capacitance = routing_layer->get_edge_capacitance();
 
   segment_capacitance =
       (lef_capacitance * segment_length * (*segment_width)) +
