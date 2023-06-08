75462772ba7b7b0c801411d803e830f2d69fb766
zhishengzeng <zhishengzeng@163.com>
2023-06-08 16:56:42 +0800

    update getWireCost

--- src/operation/iRT/source/module/detailed_router/DetailedRouter.hpp
+++ src/operation/iRT/source/module/detailed_router/DetailedRouter.hpp
@@ -130,12 +130,11 @@ class DetailedRouter
   DRNode* popFromOpenList(DRBox& dr_box);
   double getKnowCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
   double getJointCost(DRBox& dr_box, DRNode* curr_node, Orientation orientation);
-  double getKnowWireCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
+  double getWireCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
   double getKnowCornerCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
   double getViaCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
   double getEstimateCostToEnd(DRBox& dr_box, DRNode* curr_node);
   double getEstimateCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
-  double getEstimateWireCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
   double getEstimateCornerCost(DRBox& dr_box, DRNode* start_node, DRNode* end_node);
 #endif
 
