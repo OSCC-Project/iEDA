977e0a2caff8e83141bd110b55b616f350e47984
Yell-walkalone <12112088@qq.com>
2023-08-01 15:54:23 +0800

    update sky130 script & change foundry repo

--- scripts/sky130/script/DB_script/db_path_setting.tcl
+++ scripts/sky130/script/DB_script/db_path_setting.tcl
@@ -6,8 +6,8 @@ set CELL_TYPE "HS"
 #===========================================================
 ##   set tech lef path
 #===========================================================
-set TECH_LEF_HS_PATH "./lef/sky130_fd_sc_hs.tlef"
-set TECH_LEF_HD_PATH "./lef/sky130_fd_sc_hd.tlef"
+set TECH_LEF_HS_PATH "./../foundry/sky130/lef/sky130_fd_sc_hs.tlef"
+set TECH_LEF_HD_PATH "./../foundry/sky130/lef/sky130_fd_sc_hd.tlef"
 
 if { $CELL_TYPE == "HS" } {
     set TECH_LEF_PATH $TECH_LEF_HS_PATH
@@ -17,63 +17,63 @@ if { $CELL_TYPE == "HS" } {
 #===========================================================
 ##   set lef path
 #===========================================================                             
-set LEF_HS_PATH "./lef/sky130_fd_sc_hs_merged.lef \
-            ./lef/sky130_ef_io__com_bus_slice_10um.lef \
-            ./lef/sky130_ef_io__com_bus_slice_1um.lef \
-            ./lef/sky130_ef_io__com_bus_slice_20um.lef \
-            ./lef/sky130_ef_io__com_bus_slice_5um.lef \
-            ./lef/sky130_ef_io__connect_vcchib_vccd_and_vswitch_vddio_slice_20um.lef \
-            ./lef/sky130_ef_io__corner_pad.lef \
-            ./lef/sky130_ef_io__disconnect_vccd_slice_5um.lef \
-            ./lef/sky130_ef_io__disconnect_vdda_slice_5um.lef \
-            ./lef/sky130_ef_io__gpiov2_pad_wrapped.lef \
-            ./lef/sky130_ef_io__vccd_hvc_pad.lef \
-            ./lef/sky130_ef_io__vccd_lvc_pad.lef \
-            ./lef/sky130_ef_io__vdda_hvc_pad.lef \
-            ./lef/sky130_ef_io__vdda_lvc_pad.lef \
-            ./lef/sky130_ef_io__vddio_hvc_pad.lef \
-            ./lef/sky130_ef_io__vddio_lvc_pad.lef \
-            ./lef/sky130_ef_io__vssa_hvc_pad.lef \
-            ./lef/sky130_ef_io__vssa_lvc_pad.lef \
-            ./lef/sky130_ef_io__vssd_hvc_pad.lef \
-            ./lef/sky130_ef_io__vssd_lvc_pad.lef \
-            ./lef/sky130_ef_io__vssio_hvc_pad.lef \
-            ./lef/sky130_ef_io__vssio_lvc_pad.lef \
-            ./lef/sky130_fd_io__top_xres4v2.lef \
-            ./lef/sky130io_fill.lef \
-            ./lef/sky130_sram_1rw1r_128x256_8.lef \
-            ./lef/sky130_sram_1rw1r_44x64_8.lef \
-            ./lef/sky130_sram_1rw1r_64x256_8.lef \
-            ./lef/sky130_sram_1rw1r_80x64_8.lef"
+set LEF_HS_PATH "./../foundry/sky130/lef/sky130_fd_sc_hs_merged.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_10um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_1um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_20um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_5um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__connect_vcchib_vccd_and_vswitch_vddio_slice_20um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__corner_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__disconnect_vccd_slice_5um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__disconnect_vdda_slice_5um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__gpiov2_pad_wrapped.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vccd_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vccd_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vdda_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vdda_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vddio_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vddio_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssa_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssa_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssd_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssd_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssio_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssio_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_fd_io__top_xres4v2.lef \
+            ./../foundry/sky130/lef/sky130io_fill.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_128x256_8.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_44x64_8.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_64x256_8.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_80x64_8.lef"
 
-set LEF_HD_PATH "./lef/sky130_fd_sc_hd_merged.lef \
-              ./lef/sky130_ef_io__com_bus_slice_10um.lef \
-            ./lef/sky130_ef_io__com_bus_slice_1um.lef \
-            ./lef/sky130_ef_io__com_bus_slice_20um.lef \
-            ./lef/sky130_ef_io__com_bus_slice_5um.lef \
-            ./lef/sky130_ef_io__connect_vcchib_vccd_and_vswitch_vddio_slice_20um.lef \
-            ./lef/sky130_ef_io__corner_pad.lef \
-            ./lef/sky130_ef_io__disconnect_vccd_slice_5um.lef \
-            ./lef/sky130_ef_io__disconnect_vdda_slice_5um.lef \
-            ./lef/sky130_ef_io__gpiov2_pad_wrapped.lef \
-            ./lef/sky130_ef_io__vccd_hvc_pad.lef \
-            ./lef/sky130_ef_io__vccd_lvc_pad.lef \
-            ./lef/sky130_ef_io__vdda_hvc_pad.lef \
-            ./lef/sky130_ef_io__vdda_lvc_pad.lef \
-            ./lef/sky130_ef_io__vddio_hvc_pad.lef \
-            ./lef/sky130_ef_io__vddio_lvc_pad.lef \
-            ./lef/sky130_ef_io__vssa_hvc_pad.lef \
-            ./lef/sky130_ef_io__vssa_lvc_pad.lef \
-            ./lef/sky130_ef_io__vssd_hvc_pad.lef \
-            ./lef/sky130_ef_io__vssd_lvc_pad.lef \
-            ./lef/sky130_ef_io__vssio_hvc_pad.lef \
-            ./lef/sky130_ef_io__vssio_lvc_pad.lef \
-            ./lef/sky130_fd_io__top_xres4v2.lef \
-            ./lef/sky130io_fill.lef \
-            ./lef/sky130_sram_1rw1r_128x256_8.lef \
-            ./lef/sky130_sram_1rw1r_44x64_8.lef \
-            ./lef/sky130_sram_1rw1r_64x256_8.lef \
-            ./lef/sky130_sram_1rw1r_80x64_8.lef"
+set LEF_HD_PATH "./../foundry/sky130/lef/sky130_fd_sc_hd_merged.lef \
+              ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_10um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_1um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_20um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__com_bus_slice_5um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__connect_vcchib_vccd_and_vswitch_vddio_slice_20um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__corner_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__disconnect_vccd_slice_5um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__disconnect_vdda_slice_5um.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__gpiov2_pad_wrapped.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vccd_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vccd_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vdda_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vdda_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vddio_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vddio_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssa_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssa_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssd_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssd_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssio_hvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_ef_io__vssio_lvc_pad.lef \
+            ./../foundry/sky130/lef/sky130_fd_io__top_xres4v2.lef \
+            ./../foundry/sky130/lef/sky130io_fill.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_128x256_8.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_44x64_8.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_64x256_8.lef \
+            ./../foundry/sky130/lef/sky130_sram_1rw1r_80x64_8.lef"
 
 if { $CELL_TYPE == "HS" } {
     set LEF_PATH ${LEF_HS_PATH}
@@ -84,19 +84,19 @@ if { $CELL_TYPE == "HS" } {
 #===========================================================
 ##   set common lib path
 #===========================================================
-set LIB_HS_PATH "./lib/sky130_fd_sc_hs__tt_025C_1v80.lib \
-      ./lib/sky130_dummy_io.lib \
-      ./lib/sky130_sram_1rw1r_128x256_8_TT_1p8V_25C.lib \
-      ./lib/sky130_sram_1rw1r_44x64_8_TT_1p8V_25C.lib \
-      ./lib/sky130_sram_1rw1r_64x256_8_TT_1p8V_25C.lib \
-      ./lib/sky130_sram_1rw1r_80x64_8_TT_1p8V_25C.lib"
+set LIB_HS_PATH "./../foundry/sky130/lib/sky130_fd_sc_hs__tt_025C_1v80.lib \
+      ./../foundry/sky130/lib/sky130_dummy_io.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_128x256_8_TT_1p8V_25C.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_44x64_8_TT_1p8V_25C.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_64x256_8_TT_1p8V_25C.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_80x64_8_TT_1p8V_25C.lib"
 
-set LIB_HD_PATH "./lib/sky130_fd_sc_hd__tt_025C_1v80.lib \
-      ./lib/sky130_dummy_io.lib \
-      ./lib/sky130_sram_1rw1r_128x256_8_TT_1p8V_25C.lib \
-      ./lib/sky130_sram_1rw1r_44x64_8_TT_1p8V_25C.lib \
-      ./lib/sky130_sram_1rw1r_64x256_8_TT_1p8V_25C.lib \
-      ./lib/sky130_sram_1rw1r_80x64_8_TT_1p8V_25C.lib"
+set LIB_HD_PATH "./../foundry/sky130/lib/sky130_fd_sc_hd__tt_025C_1v80.lib \
+      ./../foundry/sky130/lib/sky130_dummy_io.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_128x256_8_TT_1p8V_25C.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_44x64_8_TT_1p8V_25C.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_64x256_8_TT_1p8V_25C.lib \
+      ./../foundry/sky130/lib/sky130_sram_1rw1r_80x64_8_TT_1p8V_25C.lib"
 
 if { $CELL_TYPE == "HS" } {
     set LIB_PATH ${LIB_HS_PATH}
@@ -127,10 +127,10 @@ set LIB_PATH_SETUP ${LIB_PATH}
 #===========================================================
 ##   set sdc path
 #===========================================================
-set SDC_PATH "./sdc/gcd.sdc"
-#set SDC_PATH "./sdc/uart.sdc"
-#set SDC_PATH "./sdc/aes_cipher_top.sdc"
+set SDC_PATH "./../foundry/sky130/sdc/gcd.sdc"
+#set SDC_PATH "./../foundry/sky130/sdc/uart.sdc"
+#set SDC_PATH "./../foundry/sky130/sdc/aes_cipher_top.sdc"
 #===========================================================
 ##   set spef path
 #===========================================================
-#set SPEF_PATH "./spef/gcd.spef"
+#set SPEF_PATH "./../foundry/sky130/spef/gcd.spef"
