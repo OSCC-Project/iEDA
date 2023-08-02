d3e65c8f6365d4e05a4359f6f58d4048e5864a1c
longshy <1253947628@qq.com>
2023-08-02 10:16:21 +0800

    refactor findStartOrEnd

--- src/operation/iSTA/source/module/sta/StaFindStartOrEnd.hh
+++ src/operation/iSTA/source/module/sta/StaFindStartOrEnd.hh
@@ -17,7 +17,7 @@
 /**
  * @file StaFindStartOrEnd.hh
  * @author shy long (longshy@pcl.ac.cn)
- * @brief
+ * @brief The class for finding start or end points of the timing path.
  * @version 0.1
  * @date 2023-07-21
  */
