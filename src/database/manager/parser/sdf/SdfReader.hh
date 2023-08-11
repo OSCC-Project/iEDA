d0601386cb2095e8b6cfb3ea1ec68ac4a3fbd0ec
simintao <simintao@126.com>
2023-08-11 17:53:51 +0800

    refactor:remove unused code

--- src/database/manager/parser/sdf/SdfReader.hh
+++ src/database/manager/parser/sdf/SdfReader.hh
@@ -29,7 +29,6 @@
 #include <optional>
 #include <vector>
 
-#include "DisallowCopyAssign.hh"
 #include "string/Str.hh"
 
 using ieda::Str;
@@ -52,8 +51,6 @@ class SdfTripleValue
 
  private:
   std::array<std::optional<float>, 3> _triple_value;  //!< The triple value fast:typical:slow.
-
-  DISALLOW_COPY_AND_ASSIGN(SdfTripleValue);
 };
 
 /**
@@ -216,8 +213,6 @@ class SdfReader
 
   unsigned _parse_timing_check : 1;
   unsigned _reserverd : 31;
-
-  DISALLOW_COPY_AND_ASSIGN(SdfReader);
 };
 
 }  // namespace ista
