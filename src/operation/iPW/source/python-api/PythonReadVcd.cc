3cf7d277e8c5963fcefce5350dc3304928a36086
simintao <simintao@126.com>
2023-09-10 11:22:29 +0800

    refactor:add license and change lib name

--- src/operation/iPW/source/python-api/PythonReadVcd.cc
+++ src/operation/iPW/source/python-api/PythonReadVcd.cc
@@ -1,3 +1,19 @@
+// ***************************************************************************************
+// Copyright (c) 2023-2025 Peng Cheng Laboratory
+// Copyright (c) 2023-2025 Institute of Computing Technology, Chinese Academy of
+// Sciences Copyright (c) 2023-2025 Beijing Institute of Open Source Chip
+//
+// iEDA is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan
+// PSL v2. You may obtain a copy of Mulan PSL v2 at:
+// http://license.coscl.org.cn/MulanPSL2
+//
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+//
+// See the Mulan PSL v2 for more details.
+// ***************************************************************************************
 /**
  * @file PythonReadVcd.cc
  * @author shaozheqing (707005020@qq.com)
@@ -14,6 +30,15 @@
 #include "sta/Sta.hh"
 
 namespace ipower {
+
+/**
+ * @brief interface for python of read vcd.
+ *
+ * @param vcd_file
+ * @param top_instance_name
+ * @return true
+ * @return false
+ */
 bool read_vcd(std::string vcd_file, std::string top_instance_name) {
   ista::Sta* ista = ista::Sta::getOrCreateSta();
   ipower::Power* ipower = ipower::Power::getOrCreatePower(&(ista->get_graph()));
--- src/operation/iPW/source/python-api/PythonReportPower.cc
+++ src/operation/iPW/source/python-api/PythonReportPower.cc
@@ -1,3 +1,19 @@
+// ***************************************************************************************
+// Copyright (c) 2023-2025 Peng Cheng Laboratory
+// Copyright (c) 2023-2025 Institute of Computing Technology, Chinese Academy of
+// Sciences Copyright (c) 2023-2025 Beijing Institute of Open Source Chip
+//
+// iEDA is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan
+// PSL v2. You may obtain a copy of Mulan PSL v2 at:
+// http://license.coscl.org.cn/MulanPSL2
+//
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+//
+// See the Mulan PSL v2 for more details.
+// ***************************************************************************************
 /**
  * @file .cc
  * @author shaozheqing (707005020@qq.com)
@@ -16,6 +32,11 @@
 
 namespace ipower {
 
+/**
+ * @brief interface for python of report power.
+ *
+ * @return unsigned
+ */
 unsigned report_power() {
   ista::Sta* ista = ista::Sta::getOrCreateSta();
   ipower::Power* ipower = ipower::Power::getOrCreatePower(&(ista->get_graph()));
