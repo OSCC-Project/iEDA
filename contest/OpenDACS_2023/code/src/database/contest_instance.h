42d69b8cd16b89f06dda0fb9ed4214e68a11d5e3
zhishengzeng <zhishengzeng@163.com>
2023-09-19 15:59:37 +0800

    update

--- contest/OpenDACS_2023/code/src/database/contest_instance.h
+++ contest/OpenDACS_2023/code/src/database/contest_instance.h
@@ -14,36 +14,41 @@
 //
 // See the Mulan PSL v2 for more details.
 // ***************************************************************************************
-#pragma once
 /**
- * @File Name: guide_parser.h
+ * @File Name: contest_guide.cpp
  * @Brief :
  * @Author : Yell (12112088@qq.com)
  * @Version : 1.0
  * @Creat Date : 2023-09-15
  *
  */
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma once
+#include <set>
 #include <string>
 #include <vector>
 
-#include "contest_net.h"
+#include "contest_coord.h"
 
 namespace ieda_contest {
-class ContestNet;
 
-class GuideParser
+class ContestInstance
 {
  public:
-  GuideParser() = default;
-  ~GuideParser() = default;
-
-  bool parse(std::string guide_file, std::vector<ContestNet>& net_list);
-  bool save(std::string guide_file, std::vector<ContestNet>& net_list);
+  ContestInstance() = default;
+  ~ContestInstance() = default;
+  // getter
+  std::string& get_name() { return _name; }
+  ContestCoord& get_coord() { return _coord; }
+  int get_area() const { return _area; }
+  // setter
+  void set_name(const std::string& name) { _name = name; }
+  void set_coord(const ContestCoord& coord) { _coord = coord; }
+  void set_area(const int area) { _area = area; }
 
  private:
+  std::string _name;
+  ContestCoord _coord;
+  int _area;
 };
 
 }  // namespace ieda_contest
