57b10e7de262c32d681edda8b32947dd8a17862d
simintao <simintao@126.com>
2023-10-10 11:21:42 +0800

    feature:support ipw tcl

--- src/platform/tool_manager/tool_api/ipw_io/ipw_io.h
+++ src/platform/tool_manager/tool_api/ipw_io/ipw_io.h
@@ -25,11 +25,7 @@
 #include <string>
 #include <vector>
 
-#define staInst iplf::StaIO::getInstance()
-
-namespace ista {
-class StaClockTree;
-}
+#define powerInst iplf::PowerIO::getInstance()
 
 namespace idb {
 class IdbBuilder;
@@ -38,13 +34,13 @@ enum class IdbConnectType : uint8_t;
 
 namespace iplf {
 
-class StaIO
+class PowerIO
 {
  public:
-  static StaIO* getInstance()
+  static PowerIO* getInstance()
   {
     if (!_instance) {
-      _instance = new StaIO;
+      _instance = new PowerIO;
     }
     return _instance;
   }
@@ -52,38 +48,16 @@ class StaIO
   /// getter
 
   /// io
-  bool autoRunSTA(std::string path = "");
-  bool initSTA(std::string path = "");
-  bool isInitSTA();
-  unsigned buildGraph();
-  bool runSTA(std::string path = "");
-  unsigned updateTiming();
-  bool buildClockTree(std::string sta_path = "");
-
-  std::vector<std::unique_ptr<ista::StaClockTree>>& getClockTree();
-
-  /// operator
-  std::vector<std::string> getClockNetNameList();
-  std::vector<std::string> getClockNameList();
-  std::string getCellType(const char* cell_name);
-  bool isClockNet(std::string net_name);
-  bool isSequentialCell(std::string instance_name);
-  bool insertBuffer(std::pair<std::string, std::string>& source_sink_net, std::vector<std::string>& sink_pin_list,
-                    std::pair<std::string, std::string>& master_inst_buffer, std::pair<int, int> buffer_center_loc,
-                    idb::IdbConnectType connect_type);
-
-  bool setStaWorkDirectory(std::string path = "");
-  bool readIdb(idb::IdbBuilder* idb_builder = nullptr);
-  bool runSDC(std::string path = "");
-  bool runLiberty(std::vector<std::string> paths);
-  bool runSpef(std::string path = "");
-  bool reportTiming();
+  bool autoRunPower(std::string path = "");
+  bool runPower(std::string path = "");
 
  private:
-  static StaIO* _instance;
+  static PowerIO* _instance;
 
-  StaIO() {}
-  ~StaIO() = default;
+  PowerIO() {}
+  ~PowerIO() = default;
+
+  bool reportPower();
 };
 
 }  // namespace iplf
