0126e5f303da9940480309d888df1fe7ff917727
longshy <1253947628@qq.com>
2023-10-26 19:14:03 +0800

    add program run_time

--- src/database/manager/parser/verilog/verilog-rust/verilog-parser/src/main.rs
+++ src/database/manager/parser/verilog/verilog-rust/verilog-parser/src/main.rs
@@ -1,10 +1,20 @@
 mod verilog_parser;
+use std::time::{Instant, Duration};
 
 fn main() {
+    let start_time = Instant::now();
+
     let verilog_file_str =
         // "/home/longshuaiying/iEDA/src/database/manager/parser/verilog/verilog-rust/verilog-parser/example/example1.v";
         // "/home/longshuaiying/iEDA/src/database/manager/parser/verilog/verilog-rust/verilog-parser/example/asic_top_DC_downsize.v";
         "/home/longshuaiying/iEDA/src/database/manager/parser/verilog/verilog-rust/verilog-parser/example/asic_top_flatten.v";
     let verilog_module = verilog_parser::parse_verilog_file(verilog_file_str);
     println!("{:#?}", verilog_module);
+
+    let end_time = Instant::now();
+    let elapsed_time = end_time.duration_since(start_time);
+    let elapsed_s = elapsed_time.as_secs();
+
+    println!("Program execution time (milliseconds): {} ms", elapsed_s);
+
 }
