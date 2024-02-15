147fcb69c0ed3bfcb970a77514f20efa6a72559d
黄富兴 <fxxhuang@gmail.com>
2024-02-15 15:39:00 +0000

    Revert "feat: add dependency"
    This reverts commit afd4e8b5a4a9cdd843b21a6009ed8a682160b6eb.

--- src/third_party/mt-kahypar-1.4/scripts/download_tbb_linux.cmake
+++
@@ -1,7 +0,0 @@
-include(FetchContent)
-FetchContent_Populate(
-  tbb
-  URL https://github.com/oneapi-src/oneTBB/releases/download/v2021.7.0/oneapi-tbb-2021.7.0-lin.tgz
-  URL_HASH SHA256=3c2b3287c595e2bb833c025fcd271783963b7dfae8dc681440ea6afe5d550e6a
-  SOURCE_DIR external_tools/tbb
-)
--- src/third_party/mt-kahypar-1.4/scripts/download_tbb_windows.cmake
+++
@@ -1,7 +0,0 @@
-include(FetchContent)
-FetchContent_Populate(
-  tbb
-  URL https://github.com/oneapi-src/oneTBB/releases/download/v2021.8.0/oneapi-tbb-2021.8.0-win.zip
-  URL_HASH SHA256=b9265d4dc5b74e27176c6a6b696882935f605191d014a62c010c9610904e7f65  
-  SOURCE_DIR external_tools/tbb
-)
