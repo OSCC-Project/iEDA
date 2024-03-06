c4b94a020d3b1ae5da1b1a847afea7f974f74223
simintao <simintao@126.com>
2024-03-06 17:47:18 +0800

    fix:read verilog not use cell name

--- src/operation/iSTA/source/module/shell-cmd/CmdReadVerilog.cc
+++ src/operation/iSTA/source/module/shell-cmd/CmdReadVerilog.cc
@@ -28,17 +28,12 @@ namespace ista {
 CmdReadVerilog::CmdReadVerilog(const char* cmd_name) : TclCmd(cmd_name) {
   auto* file_name_option = new TclStringOption("file_name", 1, nullptr);
   addOption(file_name_option);
-
-  auto* cell_name_option = new TclStringOption("cell_name", 1, nullptr);
-  addOption(cell_name_option);
 }
 
 unsigned CmdReadVerilog::check() {
   TclOption* file_name_option = getOptionOrArg("file_name");
   LOG_FATAL_IF(!file_name_option);
 
-  TclOption* cell_name_option = getOptionOrArg("cell_name");
-  LOG_FATAL_IF(!cell_name_option);
   return 1;
 }
 
@@ -50,13 +45,10 @@ unsigned CmdReadVerilog::exec() {
   TclOption* file_name_option = getOptionOrArg("file_name");
   auto* verilog_file = file_name_option->getStringVal();
 
-  TclOption* cell_name_option = getOptionOrArg("cell_name");
-  auto* cell_name = cell_name_option->getStringVal();
-
   Sta* ista = Sta::getOrCreateSta();
 
-  // ista->readVerilog(verilog_file);
-  ista->readVerilogWithRustParser(verilog_file, cell_name);
+  ista->readVerilog(verilog_file);
+  // ista->readVerilogWithRustParser(verilog_file, cell_name);
 
   return 1;
 }
