6bf218237b373cdc790920f8cc56284893d3a527
simintao <simintao@126.com>
2024-04-12 09:27:56 +0800

    feature:support iIR C test

---
+++ src/operation/iIR/source/iir-rust/IRRustC.cc
@@ -0,0 +1,21 @@
+/**
+ * @file IRRustC.cc
+ * @author simin tao (taosm@pcl.ac.cn)
+ * @brief The C wrapper for iIR Rust.
+ * @version 0.1
+ * @date 2024-04-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include "IRRustC.hh"
+
+namespace iir {
+
+void BuildMatrixFromRawData(const char *c_inst_power_path,
+                            const char *c_power_net_spef) {
+                                
+                            }
+
+}
+
