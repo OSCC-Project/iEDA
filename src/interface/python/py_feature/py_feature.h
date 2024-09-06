18201085843c4862982fa6563612ebc5ebb15f8b
dawnli139 <dawnli619215645@gmail.com>
2024-09-06 16:52:10 +0800

    fix: eval_pro::timing python register

--- src/interface/python/py_feature/py_feature.h
+++ src/interface/python/py_feature/py_feature.h
@@ -25,7 +25,7 @@ bool feature_tool(const std::string& path, const std::string& step);
 bool feature_eval_map(const std::string& path, const int& bin_cnt_x, const int& bin_cnt_y);
 bool feature_route(const std::string& path);
 bool feature_route_read(const std::string& path);
-bool feature_eval_summary(const std::string& path, int32_t grid_size, const std::string& routing_type);
-bool feature_timing_eval_summary(const std::string& path, const std::string& routing_type);
+bool feature_eval_summary(const std::string& path, int32_t grid_size);
+bool feature_timing_eval_summary(const std::string& path);
 
 }  // namespace python_interface
