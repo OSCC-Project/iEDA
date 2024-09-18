f14a49f6e0d9ac8ec212c89c8107f1a5099e2a42
Yell-walkalone <12112088@qq.com>
2024-09-18 11:14:03 +0800

    iEval : change eval pro to eval lib

--- src/operation/iPL/external_libs/ipl_api_external_libs.cmake
+++ src/operation/iPL/external_libs/ipl_api_external_libs.cmake
@@ -3,10 +3,10 @@ add_library(ipl-api_external_libs INTERFACE)
 find_package(OpenMP REQUIRED)
 target_link_libraries(ipl-api_external_libs
     INTERFACE
-        eval_pro_congestion_api
-        eval_pro_wirelength_api
-        eval_pro_timing_api
-        eval_pro_density_api
+        eval_congestion_api
+        eval_wirelength_api
+        eval_timing_api
+        eval_density_api
         tool_manager
         idm
         OpenMP::OpenMP_CXX
