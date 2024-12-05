b4eb4dc03f21374e76e7b950b434b88bfc48ef06
dawnli139 <dawnli619215645@gmail.com>
2024-12-05 16:50:41 +0800

    update: ilm adds example

--- src/large_model/src/graph/data_manager/lm_graph_dm.cpp
+++ src/large_model/src/graph/data_manager/lm_graph_dm.cpp
@@ -19,8 +19,8 @@
 
 #include "Log.hh"
 #include "lm_graph_check.hh"
-#include "lm_net_graph_gen.hh"
 #include "lm_graph_init.h"
+#include "lm_net_graph_gen.hh"
 #include "omp.h"
 #include "usage.hh"
 
@@ -30,6 +30,25 @@ bool LmGraphDataManager::buildGraphData(const std::string path)
 {
   LmNetGraphGenerator gen;
   auto wire_graphs = gen.buildGraphs();
+
+  std::ranges::for_each(wire_graphs, [&](auto& wire_graph) -> void {
+    // travelsal the edge in wire_graph
+    for (auto edge : boost::make_iterator_range(boost::edges(wire_graph))) {
+      auto source = boost::source(edge, wire_graph);
+      auto target = boost::target(edge, wire_graph);
+      auto source_label = wire_graph[source];
+      auto target_label = wire_graph[target];
+      LOG_INFO << "Edge: " << source_label.x << "," << source_label.y << " " << source_label.layer_id << " " << target_label.x << " "
+               << target_label.y << " " << target_label.layer_id;
+      LOG_INFO << "Path: ";
+      std::ranges::for_each(wire_graph[edge].path, [&](auto& point_pair) -> void {
+        auto start_point = point_pair.first;
+        auto end_point = point_pair.second;
+        LOG_INFO << "(" << bg::get<0>(start_point) << "," << bg::get<1>(start_point) << "," << bg::get<2>(start_point) << ") - ("
+                 << bg::get<0>(end_point) << "," << bg::get<1>(end_point) << "," << bg::get<2>(end_point) << ")";
+      });
+    }
+  });
 }
 
 }  // namespace ilm
