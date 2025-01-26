8250a53f7bd0e96df5cb137b09b8b0ad44db7514
longshy <1253947628@qq.com>
2025-01-26 18:01:04 +0800

    remove unused code

--- src/operation/iSTA/test/main.cc
+++ src/operation/iSTA/test/main.cc
@@ -20,6 +20,6 @@
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
 
-  testing::GTEST_FLAG(filter) = "SSTATest.test*";
+  testing::GTEST_FLAG(filter) = "LibDataGPUTest.test*";
   return RUN_ALL_TESTS();
 }
