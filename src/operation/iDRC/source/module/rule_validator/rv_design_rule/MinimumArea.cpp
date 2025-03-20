d52a63f5771df56427697ae5823ede1efe8db5c6
jixiao <2655432410@qq.com>
2025-03-20 11:27:11 +0800

    optmize minarea

--- src/operation/iDRC/source/module/rule_validator/rv_design_rule/MinimumArea.cpp
+++ src/operation/iDRC/source/module/rule_validator/rv_design_rule/MinimumArea.cpp
@@ -22,25 +22,19 @@ void RuleValidator::verifyMinimumArea(RVBox& rv_box)
 {
   std::vector<RoutingLayer>& routing_layer_list = DRCDM.getDatabase().get_routing_layer_list();
   std::vector<Violation>& violation_list = rv_box.get_violation_list();
-
+  int32_t min_area_drc = 0;
   std::map<int32_t, std::map<int32_t, GTLPolySetInt>> layer_net_poly_set;
-  for (DRCShape* rect : rv_box.get_drc_env_shape_list()) {
-    if (!rect->get_is_routing()) {
-      continue;
-    }
-    int32_t net_idx = rect->get_net_idx();
-    int32_t layer_idx = rect->get_layer_idx();
-    layer_net_poly_set[layer_idx][net_idx] += GTLRectInt(rect->get_ll_x(), rect->get_ll_y(), rect->get_ur_x(), rect->get_ur_y());
-  }
+  // min area只需要考虑routing shape的情况
+  // 构建boost polygon环境
   for (DRCShape* rect : rv_box.get_drc_result_shape_list()) {
-    if (!rect->get_is_routing()) {
+    if (!rect->get_is_routing() || rect->get_net_idx() == -1) {  // 不是routing layer或者net_idx为-1的跳过该检测
       continue;
     }
     int32_t net_idx = rect->get_net_idx();
     int32_t layer_idx = rect->get_layer_idx();
     layer_net_poly_set[layer_idx][net_idx] += GTLRectInt(rect->get_ll_x(), rect->get_ll_y(), rect->get_ur_x(), rect->get_ur_y());
   }
-
+  // 遍历每个图形，如果面积小于min area则添加到violation list中
   for (auto& [layer_idx, net_poly_set] : layer_net_poly_set) {
     for (auto& [net_idx, poly_set] : net_poly_set) {
       std::vector<GTLPolyInt> poly_list;
@@ -48,12 +42,15 @@ void RuleValidator::verifyMinimumArea(RVBox& rv_box)
       int32_t min_area = routing_layer_list[layer_idx].get_min_area();
       for (GTLPolyInt poly : poly_list) {
         if (gtl::area(poly) < min_area) {
-          GTLRectInt violation_rect;
-          gtl::extents(violation_rect, poly);  // 可以用max_rect代替
-          int llx = gtl::xl(violation_rect);
-          int lly = gtl::yl(violation_rect);
-          int urx = gtl::xh(violation_rect);
-          int ury = gtl::yh(violation_rect);
+          std::vector<GTLRectInt> violation_rect_list;
+          gtl::get_max_rectangles(violation_rect_list, poly);
+          std::sort(violation_rect_list.begin(), violation_rect_list.end(),
+                    [](const GTLRectInt& a, const GTLRectInt& b) { return gtl::area(a) > gtl::area(b); });
+
+          int llx = gtl::xl(violation_rect_list[0]);
+          int lly = gtl::yl(violation_rect_list[0]);
+          int urx = gtl::xh(violation_rect_list[0]);
+          int ury = gtl::yh(violation_rect_list[0]);
 
           std::set<int32_t> net_set;
           net_set.insert(net_idx);
@@ -67,11 +64,12 @@ void RuleValidator::verifyMinimumArea(RVBox& rv_box)
           violation.set_rect(PlanarRect(llx, lly, urx, ury));
 
           violation_list.push_back(violation);
+          min_area_drc += 1;
         }
       }
     }
   }
-  //   DRCLOG.info(Loc::current(),"min area num: ",violation_list.size());
+  //   DRCLOG.info(Loc::current(),"min area num: ",min_area_drc);
 }
 
 }  // namespace idrc
