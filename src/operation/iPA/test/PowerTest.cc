0bd80ffdf2e2a373bc8c7e31655d800f3dcbb422
simintao <simintao@126.com>
2025-08-12 09:03:50 +0800

    fix:mask lib lef for mistake use

--- src/operation/iPA/test/PowerTest.cc
+++ src/operation/iPA/test/PowerTest.cc
@@ -22,7 +22,6 @@
 #include "iIR/source/module/power-netlist/PGNetlist.hh"
 #include "log/Log.hh"
 #include "usage/usage.hh"
-#include "iIR/source/module/power-netlist/PGNetlist.hh"
 
 using namespace ipower;
 using namespace ieda;
@@ -105,82 +104,12 @@ TEST_F(PowerTest, runIR) {
   const char* design_work_space = "/home/taosimin/ir_example/aes/rpt";
   timing_engine->set_design_work_space(design_work_space);
 
-  std::vector<const char*> lib_files{
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp40p140ssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp40p140hvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp35p140ssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp35p140lvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp40p140lvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp30p140lvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp30p140ssg0p81v125c.lib"};
+  std::vector<const char*> lib_files{};
   timing_engine->readLiberty(lib_files);
 
   timing_engine->get_ista()->set_analysis_mode(ista::AnalysisMode::kMaxMin);
 
-  std::vector<std::string> lef_files{
-      "/home/taosimin/T28/tlef/tsmcn28_9lm6X2ZUTRDL.tlef",
-      "/home/taosimin/T28/lef/PLLTS28HPMLAINT.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140opplvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140lvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140uhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140hvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140oppuhvt.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta256x32m4fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140cg.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140oppuhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140mbhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140ulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140uhvt.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x100m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140hvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140oppulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140mb.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cgcwhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140lvt.lef",
-      "/home/taosimin/T28/lef/tpbn28v_9lm.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x128m2f_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140uhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140mblvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cgcw.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140mbhvt.lef",
-      "/home/taosimin/T28/lef/tpbn28v.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x128m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140lvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140ulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140opphvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cgehvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140mb.lef",
-      "/home/taosimin/T28/lef/tphn28hpcpgv18_9lm.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x88m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140mb.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140cghvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140opp.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cghvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140oppehvt.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb2048x48m8sw_180a.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x92m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140mblvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140cg.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140opplvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cg.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140opphvt.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb512x128m4sw_180a.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x96m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140opphvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140hvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140oppuhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cguhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140opp.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb512x64m4sw_180a.lef",
-      "/home/taosimin/T28/lef/ts6n28hpcplvta2048x32m8sw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140opp.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140oppulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140ehvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140opplvt.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb8192x64m8sw_180a.lef"};
+  std::vector<std::string> lef_files{};
 
   std::string def_file = "/home/taosimin/ir_example/aes/aes.def";
   timing_engine->readDefDesign(def_file, lef_files);
@@ -198,7 +127,7 @@ TEST_F(PowerTest, runIR) {
   Power* ipower = Power::getOrCreatePower(&(ista->get_graph()));
 
   ipower->runCompleteFlow();
-  
+
   std::string power_net_name = "VDD";
   ipower->runIRAnalysis(power_net_name);
 }
@@ -211,83 +140,13 @@ TEST_F(PowerTest, estimateIR) {
   const char* design_work_space = "/home/taosimin/ir_example/aes/rpt";
   timing_engine->set_design_work_space(design_work_space);
 
-  std::vector<const char*> lib_files{
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp40p140ssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp40p140hvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp35p140ssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp35p140lvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp40p140lvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp30p140lvtssg0p81v125c.lib",
-      "/home/taosimin/T28/lib/tcbn28hpcplusbwp30p140ssg0p81v125c.lib"};
+  std::vector<const char*> lib_files{};
   timing_engine->readLiberty(lib_files);
 
   timing_engine->get_ista()->set_analysis_mode(ista::AnalysisMode::kMaxMin);
   timing_engine->get_ista()->set_n_worst_path_per_clock(1);
 
-  std::vector<std::string> lef_files{
-      "/home/taosimin/T28/tlef/tsmcn28_9lm6X2ZUTRDL.tlef",
-      "/home/taosimin/T28/lef/PLLTS28HPMLAINT.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140opplvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140lvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140uhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140hvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140oppuhvt.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta256x32m4fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140cg.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140oppuhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140mbhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140ulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140uhvt.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x100m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140hvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140oppulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140mb.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cgcwhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140lvt.lef",
-      "/home/taosimin/T28/lef/tpbn28v_9lm.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x128m2f_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140uhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140mblvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cgcw.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140mbhvt.lef",
-      "/home/taosimin/T28/lef/tpbn28v.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x128m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140lvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140ulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140opphvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cgehvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140mb.lef",
-      "/home/taosimin/T28/lef/tphn28hpcpgv18_9lm.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x88m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140mb.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140cghvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140opp.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cghvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140oppehvt.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb2048x48m8sw_180a.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x92m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140mblvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140cg.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140opplvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cg.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140opphvt.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb512x128m4sw_180a.lef",
-      "/home/taosimin/T28/lef/ts5n28hpcplvta64x96m2fw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140opphvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140hvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140oppuhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140cguhvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140opp.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb512x64m4sw_180a.lef",
-      "/home/taosimin/T28/lef/ts6n28hpcplvta2048x32m8sw_130a.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp30p140opp.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp35p140oppulvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140ehvt.lef",
-      "/home/taosimin/T28/lef/tcbn28hpcplusbwp40p140opplvt.lef",
-      "/home/taosimin/T28/lef/ts1n28hpcplvtb8192x64m8sw_180a.lef"};
+  std::vector<std::string> lef_files{};
 
   std::string def_file = "/home/taosimin/ir_example/aes/aes.def";
   timing_engine->readDefDesign(def_file, lef_files);
@@ -314,7 +173,8 @@ TEST_F(PowerTest, estimateIR) {
   // power_engine->buildPGNetWireTopo();
 
   // or read pg spef to calc rc.
-  const char* pg_spef_file_path = "/home/taosimin/ir_example/aes/aes_vdd_vss.spef";
+  const char* pg_spef_file_path =
+      "/home/taosimin/ir_example/aes/aes_vdd_vss.spef";
   power_engine->readPGSpef(pg_spef_file_path);
 
   power_engine->runIRAnalysis(power_net_name);
