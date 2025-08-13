d11513eda6b94d502d79b1866e012c41969d2e33
Yell-walkalone <12112088@qq.com>
2025-08-13 15:32:19 +0800

    add init_floorplan python api

--- src/interface/python/py_ifp/py_ifp.h
+++ src/interface/python/py_ifp/py_ifp.h
@@ -22,7 +22,7 @@
 namespace python_interface {
 
 bool fpInit(const std::string& die_area, const std::string& core_area, const std::string& core_site, const std::string& io_site,
-            const std::string& corner_site);
+            const std::string& corner_site, double core_util, double x_margin, double y_margin, double xy_ratio, double cell_area);
 bool fpMakeTracks(const std::string& layer, int x_start, int x_step, int y_start, int y_step);
 bool fpPlacePins(const std::string& layer, int width, int height, std::vector<std::string>& sides);
 bool fpPlacePort(const std::string& pin_name, int offset_x, int offset_y, int width, int height, const std::string& layer);
